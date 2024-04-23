55674b1ff925f7fe4e1e2bbc7947a844a4850532
nathan harris <nharrisanalyst@gmail.com>
2024-04-23 11:07:16 -0700

    first commit

---
+++ src/components/DataUpload/DataUpload.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import DataUpload from "./";
+
+
+
+describe("test for <DataUpload/>", ()=>{
+    it("uploades without crashing", ()=>{
+        render(<DataUpload />)
+        const mainDiv = screen.getByTestId("data-uploader")
+        expect(mainDiv).toBeInTheDocument()
+    })
+
+    it('has a input type file', ()=>{
+        render(<DataUpload />)
+        const fileUpload = screen.getByLabelText(/Upload Your Data/i) as HTMLInputElement
+        expect(fileUpload).toBeInTheDocument()
+    })
+
+    it('uploads file, sets file in state and displays a message that a file has been uploaded', async ()=>{
+        const user = userEvent.setup()
+        render(<DataUpload />)
+        const fakeCSV = new File(['example'], 'hello.csv', {type: 'text/csv'})
+        console.log('this is our fake data',fakeCSV.type)
+        const dataInput = screen.getByLabelText(/Upload Your Data/i) as HTMLInputElement;
+        
+        await act( async() => {
+            await user.upload(dataInput, fakeCSV);
+        })
+        const uploadedMessage = screen.getByText('Your data has been Uploaded');
+        
+        expect(dataInput.files[0].name).toBe('hello.csv');
+        expect(dataInput.files[0]).toBe(fakeCSV);
+        expect(uploadedMessage).toBeInTheDocument(); //this test that the state has changed
+        
+
+    })
+})
---
+++ src/components/DataUpload/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, FormEvent } from 'react';
+
+
+const DataUpload =() =>{
+    const [dataUploaded, setDataUploaded] = useState<boolean>(false);
+
+    const onUpload = (event:FormEvent<HTMLInputElement> ) =>{
+        //console.log('this is really here to be')
+        setDataUploaded(true);
+    }
+
+    const formSent = (
+        <>
+          <h3> Your data has been Uploaded</h3>
+        </>
+    )
+    return(
+    <div data-testid="data-uploader">
+        <label htmlFor="data-input" > Upload Your Data (csv or JSON):</label>
+        <input id="data-input" type="file" accept="text/csv, application/json" aria-label="data-input" onChange={onUpload}/>
+
+        {dataUploaded?formSent:null}
+
+    </div>
+    )
+}
+
+export default DataUpload
