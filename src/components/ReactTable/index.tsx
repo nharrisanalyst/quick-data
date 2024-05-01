e9f94eb126c035ab9c44e2b41fe4ece1fa58c89b
nathan harris <nharrisanalyst@gmail.com>
2024-04-30 22:37:07 -0700

    added table

--- src/components/ReactTable/index.tsx
+++ src/components/ReactTable/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { createColumns } from './utils'
 
+type ReactTableProps =  {
+    data:Array<Object>;
+}
 
-
-
-const ReactTable = () =>(
+const ReactTable = ({data=[]}:ReactTableProps) =>{
+    console.log('this is the main place', data);
+    const COLUMNS = data.length>0?createColumns(data[0]):[];
+   
+   return (
     <div data-testid="react-table">
-
+       <DataTable value={data} tableStyle={{ minWidth: '50rem' }}>
+       {COLUMNS.map((col, i) => (
+        <Column key={col.label} field={col.label} header={col.label} />
+    ))}
+       </DataTable>
     </div>
-)
+   )
+}
 
 
 export default ReactTable;
